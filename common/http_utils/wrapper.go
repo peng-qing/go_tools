d1094ce4842c2abb6b8e0e1fd5d2b708ae9a0bbd
pengqq <ispromise886@163.com>
2025-06-11 23:46:50 +0800

    feat: add http utils wrapper desc

--- common/http_utils/wrapper.go
+++ common/http_utils/wrapper.go
@@ -16,26 +16,37 @@ func init() {
 	})
 }
 
+// Get 请求
 func Get(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Get(url, options)
 }
 
+// Post 请求
 func Post(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Post(url, options)
 }
 
+// Delete 删除请求
 func Delete(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Delete(url, options)
 }
+
+// Put 请求
 func Put(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Put(url, options)
 }
+
+// Patch 请求
 func Patch(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Patch(url, options)
 }
+
+// Head 请求
 func Head(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Head(url, options)
 }
+
+// Options 获取请求
 func Options(url string, options *HttpHeader) (*http.Request, *Response, error) {
 	return session.Options(url, options)
 }
